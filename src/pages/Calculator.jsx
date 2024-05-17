21b7466819819eb402d87bdc08d3bf6e89b79f91
Ivylegend <victoryicha2@gmail.com>
2024-05-17 15:57:27 +0100

    removed download button from the pdf

--- src/pages/Calculator.jsx
+++ src/pages/Calculator.jsx
@@ -49,143 +49,146 @@ const Calculator = () => {
   };
 
   return (
-    <div
-      className="p-10 lg:p-0 min-h-screen lg:min-h-[1200px] min-w-[100vw] flex justify-center items-center flex-col"
-      ref={targetRef}
-    >
-      <div className="border bg-white mx-auto my-8 border-black rounded-2xl p-3 md:max-w-2xl flex flex-col gap-8">
-        <div className="rounded-lg border-[0.4px] p-3 flex flex-col gap-4 bg-white">
-          <div className="flex justify-between items-center">
-            <p className="uppercase font-semibold text-sm">Tution Fee</p>
-            <input
-              type="number"
-              value={tuition}
-              onChange={(e) => setTuition(e.target.value)}
-              className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
-            />
+    <>
+      <div
+        className="p-10 lg:p-0 min-h-screen min-w-[100vw] flex justify-center items-center flex-col"
+        ref={targetRef}
+      >
+        <div className="border bg-white mx-auto my-8 border-black rounded-2xl p-3 md:max-w-2xl flex flex-col gap-8">
+          <div className="rounded-lg border-[0.4px] p-3 flex flex-col gap-4 bg-white">
+            <div className="flex justify-between items-center">
+              <p className="uppercase font-semibold text-sm">Tution Fee</p>
+              <input
+                type="number"
+                value={tuition}
+                onChange={(e) => setTuition(e.target.value)}
+                className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
+              />
+            </div>
+            <div className="flex justify-between items-center">
+              <p className="uppercase font-semibold text-sm">Hostel Fee </p>
+              <input
+                type="number"
+                value={hostel}
+                onChange={(e) => setHostel(e.target.value)}
+                className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
+              />
+            </div>
+            <div className="flex justify-between items-center">
+              <p className="uppercase font-semibold text-sm">Cost Of Living </p>
+              <input
+                type="number"
+                value={costOfLiving}
+                onChange={(e) => setCostOfLiving(e.target.value)}
+                className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
+              />
+            </div>
+            <div className="flex justify-between items-center">
+              <p className="uppercase font-semibold text-sm">other cost</p>
+              <input
+                type="number"
+                value={others}
+                onChange={(e) => setOthers(e.target.value)}
+                className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
+              />
+            </div>
           </div>
-          <div className="flex justify-between items-center">
-            <p className="uppercase font-semibold text-sm">Hostel Fee </p>
-            <input
-              type="number"
-              value={hostel}
-              onChange={(e) => setHostel(e.target.value)}
-              className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
-            />
-          </div>
-          <div className="flex justify-between items-center">
-            <p className="uppercase font-semibold text-sm">Cost Of Living </p>
-            <input
-              type="number"
-              value={costOfLiving}
-              onChange={(e) => setCostOfLiving(e.target.value)}
-              className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
-            />
-          </div>
-          <div className="flex justify-between items-center">
-            <p className="uppercase font-semibold text-sm">other cost</p>
-            <input
-              type="number"
-              value={others}
-              onChange={(e) => setOthers(e.target.value)}
-              className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
-            />
-          </div>
-        </div>
-        <div>
-          <p className="font-medium uppercase text-lg mb-4">
-            Select your flexible loan Period
-          </p>
-          <div className="flex justify-center md:justify-between items-center flex-wrap gap-4">
-            <button
-              className={`flex items-center font-semibold text-sm py-2 px-2 rounded-xl border-2 text-[#db251a] border-[#db251a] cursor-pointer ${
-                period === 6 ? "bg-red-500 text-white" : ""
-              }`}
-              onClick={() => setPeriod(6)}
-            >
-              6 months
-            </button>
-            <button
-              className={`flex items-center font-semibold text-sm py-2 px-2 rounded-xl border-2 text-[#db251a] border-[#db251a] cursor-pointer ${
-                period === 12 ? "bg-red-500 text-white" : ""
-              }`}
-              onClick={() => setPeriod(12)}
-            >
-              12 months
-            </button>
-            <button
-              className={`flex items-center font-semibold text-sm py-2 px-2 rounded-xl border-2 text-[#db251a] border-[#db251a] cursor-pointer ${
-                period === 18 ? "bg-red-500 text-white" : ""
-              }`}
-              onClick={() => setPeriod(18)}
-            >
-              18 months
-            </button>
-          </div>
-        </div>
-        <span className="flex items-center justify-end gap-4">
-          <button
-            className="font-semibold py-3 px-6 rounded-xl border-2 text-[#db251a] border-[#db251a]"
-            onClick={Reset}
-          >
-            RESET
-          </button>
-          <button
-            className="font-semibold py-3 px-6 rounded-xl border-2 bg-[#db251a] border-[#db251a] text-white"
-            onClick={() => calculateReturn()}
-          >
-            COMPUTE
-          </button>
-        </span>
-        <div className="rounded-lg border-[0.4px] p-3 flex flex-col gap-4 bg-white">
-          <div className="flex justify-between items-center">
-            <p className="uppercase font-semibold text-sm">total loan Amount</p>
-            <input
-              type="number"
-              value={totalLoan}
-              disabled
-              //   onChange={(e) => setTotal(e.target.value)}
-              className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
-            />
-          </div>
-          <div className="flex justify-between items-center gap-4">
-            <p className="uppercase font-semibold text-sm">
-              Your Monthly Payment
+          <div>
+            <p className="font-medium uppercase text-lg mb-4">
+              Select your flexible loan Period
             </p>
-            <input
-              type="text"
-              value={monthly}
-              disabled
-              className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
-            />
+            <div className="flex justify-center md:justify-between items-center flex-wrap gap-4">
+              <button
+                className={`flex items-center font-semibold text-sm py-2 px-2 rounded-xl border-2 text-[#db251a] border-[#db251a] cursor-pointer ${
+                  period === 6 ? "bg-red-500 text-white" : ""
+                }`}
+                onClick={() => setPeriod(6)}
+              >
+                6 months
+              </button>
+              <button
+                className={`flex items-center font-semibold text-sm py-2 px-2 rounded-xl border-2 text-[#db251a] border-[#db251a] cursor-pointer ${
+                  period === 12 ? "bg-red-500 text-white" : ""
+                }`}
+                onClick={() => setPeriod(12)}
+              >
+                12 months
+              </button>
+              <button
+                className={`flex items-center font-semibold text-sm py-2 px-2 rounded-xl border-2 text-[#db251a] border-[#db251a] cursor-pointer ${
+                  period === 18 ? "bg-red-500 text-white" : ""
+                }`}
+                onClick={() => setPeriod(18)}
+              >
+                18 months
+              </button>
+            </div>
           </div>
+          <span className="flex items-center justify-end gap-4">
+            <button
+              className="font-semibold py-3 px-6 rounded-xl border-2 text-[#db251a] border-[#db251a]"
+              onClick={Reset}
+            >
+              RESET
+            </button>
+            <button
+              className="font-semibold py-3 px-6 rounded-xl border-2 bg-[#db251a] border-[#db251a] text-white"
+              onClick={() => calculateReturn()}
+            >
+              COMPUTE
+            </button>
+          </span>
+          <div className="rounded-lg border-[0.4px] p-3 flex flex-col gap-4 bg-white">
+            <div className="flex justify-between items-center">
+              <p className="uppercase font-semibold text-sm">
+                total loan Amount
+              </p>
+              <input
+                type="number"
+                value={totalLoan}
+                disabled
+                //   onChange={(e) => setTotal(e.target.value)}
+                className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
+              />
+            </div>
+            <div className="flex justify-between items-center gap-4">
+              <p className="uppercase font-semibold text-sm">
+                Your Monthly Payment
+              </p>
+              <input
+                type="text"
+                value={monthly}
+                disabled
+                className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
+              />
+            </div>
 
-          <div className="flex justify-between items-center gap-4">
-            <p className="uppercase font-semibold text-sm">
-              Your Total Payment will be
-            </p>
-            <input
-              type="text"
-              value={totalPayment}
-              disabled
-              className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
-            />
-          </div>
-          <div className="flex justify-between items-center gap-4">
-            <p className="uppercase font-semibold text-sm">
-              Your Total Interest Payment Will Be
-            </p>
-            <input
-              type="text"
-              placeholder=""
-              value={interest}
-              disabled
-              className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
-            />
+            <div className="flex justify-between items-center gap-4">
+              <p className="uppercase font-semibold text-sm">
+                Your Total Payment will be
+              </p>
+              <input
+                type="text"
+                value={totalPayment}
+                disabled
+                className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
+              />
+            </div>
+            <div className="flex justify-between items-center gap-4">
+              <p className="uppercase font-semibold text-sm">
+                Your Total Interest Payment Will Be
+              </p>
+              <input
+                type="text"
+                placeholder=""
+                value={interest}
+                disabled
+                className="border w-[100px] sm:w-[200px] p-3 text-black rounded-xl"
+              />
+            </div>
           </div>
         </div>
       </div>
-
       <div className="flex justify-center items-center">
         <button
           onClick={() => toPDF()}
@@ -194,7 +197,7 @@ const Calculator = () => {
           Download as a PDF
         </button>
       </div>
-    </div>
+    </>
   );
 };
 
