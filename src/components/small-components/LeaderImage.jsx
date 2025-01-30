fca7e933f3fd178440625c42478b09d678693c06
Ivylegend <victoryicha2@gmail.com>
2025-01-30 08:33:21 +0100

    add more components to corporate profile page

--- src/components/small-components/LeaderImage.jsx
+++ src/components/small-components/LeaderImage.jsx
@@ -7,10 +7,10 @@ const LeaderImage = ({ name, position, image }) => {
       <img src={image} alt="image" className="w-full h-full object-cover" />
 
       {/* Overlay behind Text Content */}
-      <div className="absolute bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black to-transparent">
+      <div className="absolute space-y-1 bottom-0 left-0 w-full p-4 bg-gradient-to-t from-black to-transparent">
         {/* Text Content */}
         <p className="text-white font-bold">{name}</p>
-        <p className="text-white font-semibold">{position}</p>
+        <p className="text-white font-medium">{position}</p>
       </div>
     </div>
   );
