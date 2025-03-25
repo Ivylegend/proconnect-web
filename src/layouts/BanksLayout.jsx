cb451274fad805ab409efc647accf71fa28a89ea
Ivylegend <victoryicha2@gmail.com>
2025-03-25 04:19:44 +0100

    add whatsapp icon on bank layout

--- src/layouts/BanksLayout.jsx
+++ src/layouts/BanksLayout.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Footer from "../components/Footer";
 import BanksNavbar from "../components/BanksNavbar";
+import WhatsappIcon from "../components/WhatsappIcon";
 
 const BanksLayout = ({ children, logo }) => {
   return (
     <main className="bg-[#F4F4F4]">
       <BanksNavbar bankLogo={logo} />
       {children}
+      <WhatsappIcon />
       <Footer />
     </main>
   );
